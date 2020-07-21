ec0a8667dbe9ee82bfd7201519604796aab28fc0
Chris Drane <obromois@gmail.com>
2020-07-21 11:24:54 +1000

    add social_distancing_v2.pdf

--- script/release.rb
+++ script/release.rb
@@ -2,6 +2,7 @@
 # creates a new release and tag
 # In order to bundle and execute: ruby script/release.rb
 require 'bundler/inline'
+require 'colorize'
 
 gemfile do
   source 'https://rubygems.org'
@@ -11,25 +12,38 @@ end
 include ShellMethods
 
 prompt = TTY::Prompt.new
-name = prompt.select 'Select pdf file name', %w(fii systems)
-case name
-when 'fii'
-  system('cp ../covid_tracker/latex/free_infected/fii.pdf fii.pdf')
-when 'systems'
-  system('cp ../covid_tracker/latex/self_isolation/systems.pdf systems.pdf')
-end
-target = prompt.ask 'Enter target name (copy from \fancyhead minus pdf extension)'
-system("cp #{name}.pdf #{target}.pdf" )
-tag = prompt.ask 'Tag name, as shown in \fancyhead '
+name = prompt.select 'Select pdf file name:', %w(fii systems)
+copy_file_from_repo name
+target = prompt.ask 'Enter target name (copy from \fancyhead minus pdf extension):'
+system("mv #{name}.pdf #{target}.pdf" )
 git_commit "add #{target}.pdf"
-system("git tag #{tag} -a")
+system('git push origin master')
+system("open #{target}.pdf")
+yes = prompt.yes? "Does url in header of pdf got to latest release?"
+puts "You need to fix url".colorize(:red)
+exit unless yes
+tag = prompt.ask 'Tag name, as shown in \fancyhead:'
+system("git tag -a #{tag} -m 'New release'")
 system("git push origin #{tag}")
+puts 'All Done'.colorize(:green)
 
 BEGIN {
   def git_commit(msg)
     `git add .`
     `git status`
     system("git commit -m '#{msg}'")
+  end
+
+  def copy_file_from_repo(name)
+    case name
+    when 'fii'
+      system('cp ../covid_tracker/latex/free_infected/fii.pdf fii.pdf')
+    when 'systems'
+      system('cp ../covid_tracker/latex/self_isolation/systems.pdf systems.pdf')
+    else
+      puts "Need to add provision for #{name} in case statement".colorize(:red)
+      exit
+    end
   end
 }
 
